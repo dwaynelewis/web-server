3e4580178f99d18448dd48536293fa4693f1a1a2
Dwayne Richard Lewis <dwayne@fashiongatekeepers.com>
2017-08-24 15:57:36 -0400

    weekend homework

---
+++ video_game.rb
@@ -0,0 +1,101 @@
+
+   puts "Welcome to the life of THE MOST INTERESTING MAN IN THE WORLD!"
+puts "What will today's adventure be!?"
+puts "Options: A-go yachting, B-goto a social event"
+
+option = gets.chomp.downcase
+case option
+when "go yachting"
+    puts "who should I invite?"
+    puts "Options: rihanna, mommy"
+    optionriri = gets.chomp.downcase
+    case optionriri
+    when "rihanna"
+      puts "Which boat should I take?"
+      puts "The fast boat or the luxe?"
+      optionriri2 = gets.chomp.downcase
+      case optionriri2
+      when "fast boat"
+        puts "ok i should bring my goggles"
+      when "the luxe"
+        puts "i will need my robe!"
+      end
+
+    when "mommy"
+      puts "a-Should I invite dad also? or b-her church friends "
+      optionmom = gets.chomp.downcase
+      case optionmom
+
+      when "a"
+        puts "ok i will call him"
+      when "b"
+        puts "ok i will call the ladies"
+      end
+    end
+
+when "goto a social event"
+    puts "Which party has the best crowd?"
+    puts "Options: Z-the mansion party, Y-grammys afterparty"
+    optionparty = gets.chomp.downcase
+    puts optionparty
+    case optionparty when "z"
+      puts "I will wear my Versace robe!"
+    when "y"
+      puts "I will have to break the tuxe out"
+    else
+        puts "I AM GOING TO TAKE THE DAY OFF"
+    end
+end
+
+
+
+
+
+
+   puts "Welcome to the life of THE MOST INTERESTING MAN IN THE WORLD!"
+puts "What will today's adventure be!?"
+puts "Options: A-go yachting, B-goto a social event"
+
+option = gets.chomp.downcase
+case option
+when "go yachting"
+    puts "who should I invite?"
+    puts "Options: rihanna, mommy"
+    optionriri = gets.chomp.downcase
+    case optionriri
+    when "rihanna"
+      puts "Which boat should I take?"
+      puts "The fast boat or the luxe?"
+      optionriri2 = gets.chomp.downcase
+      case optionriri2
+      when "fast boat"
+        puts "ok i should bring my goggles"
+      when "the luxe"
+        puts "i will need my robe!"
+      end
+
+    when "mommy"
+      puts "a-Should I invite dad also? or b-her church friends "
+      optionmom = gets.chomp.downcase
+      case optionmom
+
+      when "a"
+        puts "ok i will call him"
+      when "b"
+        puts "ok i will call the ladies"
+      end
+    end
+
+when "goto a social event"
+    puts "Which party has the best crowd?"
+    puts "Options: Z-the mansion party, Y-grammys afterparty"
+    optionparty = gets.chomp.downcase
+    puts optionparty
+    case optionparty when "z"
+      puts "I will wear my Versace robe!"
+    when "y"
+      puts "I will have to break the tuxe out"
+    else
+        puts "I AM GOING TO TAKE THE DAY OFF"
+    end
+end
